e755fd2fa5eac1a522b225a8878cb8c0d79f9b21
NancyVazquezValladares <113449039+NancyVazquezValladares@users.noreply.github.com>
2023-04-03 20:23:39 -0600

    Add files via upload
    C#

---
+++ API REST GitHub/API REST GitHub/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace API_REST_GitHub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSolicitud = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lbRepositorio = new System.Windows.Forms.ListBox();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSolicitud
+            // 
+            this.btnSolicitud.BackColor = System.Drawing.Color.Transparent;
+            this.btnSolicitud.BackgroundImage = global::API_REST_GitHub.Properties.Resources.enviar_mensaje;
+            this.btnSolicitud.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSolicitud.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSolicitud.Location = new System.Drawing.Point(452, 90);
+            this.btnSolicitud.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSolicitud.Name = "btnSolicitud";
+            this.btnSolicitud.Size = new System.Drawing.Size(84, 72);
+            this.btnSolicitud.TabIndex = 0;
+            this.btnSolicitud.UseVisualStyleBackColor = false;
+            this.btnSolicitud.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtUsuario.Location = new System.Drawing.Point(183, 112);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(218, 30);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // lbRepositorio
+            // 
+            this.lbRepositorio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lbRepositorio.FormattingEnabled = true;
+            this.lbRepositorio.ItemHeight = 23;
+            this.lbRepositorio.Location = new System.Drawing.Point(81, 194);
+            this.lbRepositorio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lbRepositorio.Name = "lbRepositorio";
+            this.lbRepositorio.Size = new System.Drawing.Size(531, 326);
+            this.lbRepositorio.TabIndex = 2;
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.Transparent;
+            this.btnlimpiar.BackgroundImage = global::API_REST_GitHub.Properties.Resources.basura;
+            this.btnlimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnlimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlimpiar.Location = new System.Drawing.Point(134, 535);
+            this.btnlimpiar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(94, 69);
+            this.btnlimpiar.TabIndex = 3;
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.BackColor = System.Drawing.Color.Transparent;
+            this.btnsalir.BackgroundImage = global::API_REST_GitHub.Properties.Resources.cerrar_sesion;
+            this.btnsalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnsalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsalir.Location = new System.Drawing.Point(275, 535);
+            this.btnsalir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(68, 69);
+            this.btnsalir.TabIndex = 4;
+            this.btnsalir.UseVisualStyleBackColor = false;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.BackColor = System.Drawing.Color.Black;
+            this.Label1.Font = new System.Drawing.Font("Lucida Fax", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.Transparent;
+            this.Label1.Location = new System.Drawing.Point(168, 49);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(309, 22);
+            this.Label1.TabIndex = 8;
+            this.Label1.Text = "Ingresa tu Nombre de Usuario";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.BackColor = System.Drawing.Color.Black;
+            this.Label2.ForeColor = System.Drawing.Color.White;
+            this.Label2.Location = new System.Drawing.Point(77, 157);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(139, 23);
+            this.Label2.TabIndex = 11;
+            this.Label2.Text = "REPOSITORIOS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::API_REST_GitHub.Properties.Resources.Fondo_Negro_3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(672, 649);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.lbRepositorio);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnSolicitud);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "GitHub.Com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSolicitud;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.ListBox lbRepositorio;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btnsalir;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Label Label2;
+    }
+}
+
---
+++ API REST GitHub/API REST GitHub/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace API_REST_GitHub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // URL de la API REST de GitHub para buscar repositorios de un usuario específico
+                string apiUrl = "https://api.github.com/users/{username}/repos";
+                // Solicita al usuario que ingrese el nombre de usuario de GitHub del propietario del repositorio
+                string username = txtUsuario.Text.Trim(); // .Trim se utiliza para eliminar todos los espacios en blanco
+                                                          // Opcion1:txtUsuario.Text.Trim() Ingresa tu username desde el textbox
+                                                          // Opcion2: InputBox("Ingresa el nombre de usuario de GitHub del propietario del repositorio:", "Nombre de usuario")
+                 // Token
+                string token = "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";
+
+                // Cliente HTTP para enviar la solicitud a la API REST
+                HttpClient httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Mozilla", "5.0"));
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // Agrega el token de acceso personal como un encabezado de autorización en la solicitud HTTP
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                // Envia la solicitud HTTP GET a la API REST de GitHub y obtiene la respuesta
+                HttpResponseMessage response = httpClient.GetAsync(apiUrl.Replace("{username}", username)).Result;//HttpResponseMessage response = await httpClient.GetAsync(apiUrl.Replace("{username}", username));
+
+                // Si la respuesta es satisfactoria, obtiene el contenido de la respuesta como una cadena JSON
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = response.Content.ReadAsStringAsync().GetAwaiter().GetResult(); //string responseContent = await response.Content.ReadAsStringAsync();
+                    // Analiza la cadena JSON para obtener un arreglo de objetos JSON que representan los repositorios
+                    JArray repositories = JArray.Parse(responseContent);
+
+                    // Agrega el nombre de cada repositorio a la lista de repositorios en el formulario
+                    foreach (JObject repository in repositories)
+
+                        lbRepositorio.Items.Add(repository["name"]);
+                }
+                else
+                {
+
+                }
+                    MessageBox.Show("Error al buscar los repositorios: " + response.ReasonPhrase);
+            }
+            catch(Exception ex) { MessageBox.Show("Error al buscar los repositorios: " + ex.Message); }
+                }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            lbRepositorio.Items.Clear();
+            txtUsuario.Clear();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
